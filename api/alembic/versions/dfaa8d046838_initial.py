89ac386d77b4e50aea9e1beaeb98d8ce82f14331
HangoverHGV <hangobogdan@gmail.com>
2024-12-02 20:50:01 +0200

    db setup

---
+++ api/alembic/versions/dfaa8d046838_initial.py
@@ -0,0 +1,26 @@
+"""Initial
+
+Revision ID: dfaa8d046838
+Revises: 
+Create Date: 2024-12-02 18:46:23.523590
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'dfaa8d046838'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
